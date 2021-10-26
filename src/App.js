1f33130bcbc06d8cd7d12dab2d0b8e82b03ebe86
Hartaj Singh Sidhu <hartajsinghsidhu2005@gmail.com>
2021-10-26 18:14:51 +0530

    Trying to learn gsap and three.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { react, useRef, useEffect, useState } from "react";
+import { Power3, gsap } from "gsap";
+import { GamesSharp, Power, VisibilityRounded } from "@material-ui/icons";
 
 function App() {
+  let whitecircle = useRef(null);
+  let redcircle = useRef(null);
+  let bluecircle = useRef(null);
+  const [isExpaneded, setisExpaneded] = useState(false)
+
+  let App = useRef(null)
+
+  const handleExpand = ()=>{
+    if(isExpaneded){
+    gsap.to(redcircle,{duration:0.5,width: 200 , height:200 , ease:Power3.easeOut})
+    setisExpaneded(true)
+  }else{
+      gsap.to(redcircle,{duration:0.5, width:100, height:100})
+    }
+  }
+
+  useEffect(() => {
+    gsap.to(App,{css:{ visibility: "visible"}})
+    gsap.from(whitecircle, {
+      duration: 1.5,
+      opacity: 0,
+      x: 60,
+      ease: Power3.easeOut,
+    });
+    gsap.from(redcircle, {
+      duration: 1.5,
+      opacity: 0,
+      x: 60,
+      delay:0.2,
+      ease: Power3.easeOut,
+    });
+    gsap.from(bluecircle, {
+      duration: 1.5,
+      opacity: 0,
+      x: 60,
+      delay:0.4,
+      ease: Power3.easeOut,
+    });
+  
+    
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" ref={(el)=>{ App = el}}>
+      <div className="circles">
+        <div
+          className="circle yellow"
+          ref={(el) => {
+            whitecircle = el;
+          }}
+        ></div>
+        <div
+          className="circle red"
+          ref={(el) => {
+            redcircle = el;
+          }}
+          onClick={()=>{handleExpand()}}
+        ></div>
+        <div
+          className="circle blue"
+          ref={(el) => {
+            bluecircle = el;
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
